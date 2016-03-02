d45e882eabccbfa9ea2f7e3b2ef52d377f586ff0
Sean Goheen <cloud9ide@onfire.us>
2016-03-02 16:45:19 +0000

    add: version 0.0.1

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    ip      : '0.0.0.0',
+    port    : 6789,
+    tls     : false,
+    timeout : 60000,
+};
---
+++ index.js
@@ -0,0 +1,58 @@
+//var config = require('./config').config;
+var extend = require('util')._extend;
+var restify = require('restify');
+var pmb = require('pmb');
+var server = restify.createServer();
+    server.use(restify.queryParser());
+    server.use(restify.bodyParser());
+var client = pmb.client();
+
+// Response function
+function respond(req, res, next) {
+    
+    // Publish message to queue
+    var msg = extend({}, req.params);
+    msg['_role']    = 'webserver';
+    msg['_request'] = {
+        id:             req.id(),
+        headers:        req.headers,
+        httpVersion:    req.httpVersion,
+        statusCode:     req.statusCode,
+        statusMessage:  req.statusMessage,
+        url:            req.url,
+        path:           req.path(),
+        rawQuery:       req.getQuery(),
+        query:          req.query,
+        params:         req.params
+    };
+    
+    var timeout = setTimeout(function(){
+        timeout = false;
+        res.error(400, {Error: 'pattern timeout'});
+        next();
+        return;
+    },5000);
+    
+    client.send(msg, function(response, request){
+        if(!timeout) {return;}
+        clearTimeout(timeout);
+        
+        console.log("Response: ", response);
+        
+        // Reveive response and return it to client
+        res.send(response);
+        next();
+        return;
+    });
+    
+}
+
+
+// Request routing
+server.get(/.*/, respond);
+
+
+// Start server
+server.listen(8080, function() {
+    console.log('%s listening at %s', server.name, server.url);
+});
